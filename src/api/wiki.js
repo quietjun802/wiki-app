1bc1b3929b7588c22a1106f32ecc1c37b05558ff
quietjun802 <whdydwns802@gmail.com>
2025-08-11 12:33:22 +0900

    1

---
+++ src/api/wiki.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export const searchWikiTitles = async (q) => {
+
+    const res = await axios.get("https://ko.wikipedia.org/w/rest.php/v1/search/title", {
+        params: { q, limit: 5 }
+    })
+
+    return res.data.pages;
+
+}
